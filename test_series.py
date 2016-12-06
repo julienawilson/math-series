5f55e0156c8b53bd6294a194a51c4a5648a8175e
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-06 14:25:10 -0800

    added for parameters to test up to and including 10

--- test_series.py
+++ test_series.py
@@ -5,6 +5,15 @@ import pytest
 FIBONACCI_NUMBERS = [
     [0, 0],
     [1, 1],
+    [2, 1],
+    [3, 2],
+    [4, 3],
+    [5, 5],
+    [6, 8],
+    [7, 13],
+    [8, 21],
+    [9, 34],
+    [10, 55],
 ]
 
 @pytest.mark.parametrize("n, result", FIBONACCI_NUMBERS)
