6fd91793028c943d3cb3717ffc6f4538a914c12f
Julien Wilson <julienawilson@gmail.com>
2016-12-06 13:45:06 -0800

    second test fibonacci(0)

--- test_series.py
+++ test_series.py
@@ -5,3 +5,9 @@ def test_fibonacci_exists():
     """Test whether fibonacci function exists."""
     import series
     assert series.fibonacci
+
+
+def test_fib_0():
+    """Test output of fibanacci with argument 0."""
+    from series import fibonacci
+    assert fibonacci(0) == 0
