57a26e6711617b2d10074af97d7dd685615d0551
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-06 14:53:39 -0800

    added algorithm to lucas function mimicking fibonacci functionality

--- series.py
+++ series.py
@@ -11,4 +11,9 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 def lucas(n):
-    return 0
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
