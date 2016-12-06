841219e6f5f3032da67bb623f3ccf8ab3df1cfa2
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-06 15:16:45 -0800

    created function sum_series

--- series.py
+++ series.py
@@ -10,10 +10,17 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
+
 def lucas(n):
+    """Return the nth number in the lucas series."""
     if n == 0:
         return 2
     elif n == 1:
         return 1
     else:
         return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(n, a=0, b=1):
+    """Determine nth number in series with starting values of a and b."""
+    pass
