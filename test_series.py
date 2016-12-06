9af515cfc955042db2f02f1633b5529664a42006
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-06 13:59:39 -0800

    added the function to test for n in fibonacci numbers

--- test_series.py
+++ test_series.py
@@ -1,5 +1,18 @@
 """Tests for series function."""
+import pytest
 
+
+FIBONACCI_NUMBERS = [
+    [0, 0],
+    [1, 1],
+]
+
+@pytest.mark.parametrize("n, result", FIBONACCI_NUMBERS)
+def test_fibonacci(n, result):
+    """test fibonacci for some value of n."""
+    import series
+    assert series.fibonacci(n) == result
+    
 
 def test_fibonacci_exists():
     """Test whether fibonacci function exists."""
