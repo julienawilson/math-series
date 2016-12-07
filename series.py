20d7afc6483cc0c805fcc28e5885a87b8e22a3e8
Julien Wilson <julienawilson@gmail.com>
2016-12-06 17:02:02 -0800

    finished string output

--- series.py
+++ series.py
@@ -2,7 +2,7 @@
 
 
 def fibonacci(n):
-    """Return the nth number in the fibonaci series."""
+    """Return the nth number in the fibonacci series."""
     if n == 0:
         return 0
     elif n == 1:
@@ -37,12 +37,16 @@ if __name__ == "__main__":
     ...
 
     fibonacci(n):
-    Return the nth number in the fibonaci series.
+    Return the nth number in the fibonacci series.
 
     lucas(n):
     Return the nth number in the lucas series.
-    
+
     sum_series(n, first=0, second=1):
     Determine nth number in series with starting values of first and second.
     """
     print(output)
+    print("\texamples")
+    print("\t>>>fibonacci(2): ", fibonacci(2))
+    print("\t>>>lucas(3): ", lucas(3))
+    print("\t>>>sum_series(4, 3, 3): ", sum_series(4, 3, 3))
